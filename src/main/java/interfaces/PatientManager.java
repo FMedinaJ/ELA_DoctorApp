d478696524f34c2e82903f4792e509a353d5f70f
FMedinaJ <fernando.demedinajunyent@usp.ceu.es>
2025-10-24 18:01:16 +0200

    interface, jdbc and more pojos

---
+++ src/main/java/interfaces/PatientManager.java
@@ -0,0 +1,4 @@
+package interfaces;
+
+public class PatientManager {
+}
--- src/main/java/pojos/Doctor.java
+++ src/main/java/pojos/Doctor.java
@@ -1,18 +1,18 @@
-package Pojos;
+package pojos;
 
 import java.util.Date;
 import java.util.Objects;
 
 public class Doctor {
-    private int id;
+    private Integer id;
     private String name;
     private String surname;
-    private int DNI;
+    private Integer DNI;
     private Date birthDate;
     private String gender;
     public String email;
 
-    public Doctor(int id, String name, String surname, int DNI, Date birthDate, String gender, String email) {
+    public Doctor(Integer id, String name, String surname, Integer DNI, Date birthDate, String gender, String email) {
         this.id = id;
         this.name = name;
         this.surname = surname;
@@ -22,11 +22,11 @@ public class Doctor {
         this.email = email;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -46,11 +46,11 @@ public class Doctor {
         this.surname = surname;
     }
 
-    public int getDNI() {
+    public Integer getDNI() {
         return DNI;
     }
 
-    public void setDNI(int DNI) {
+    public void setDNI(Integer DNI) {
         this.DNI = DNI;
     }
 
