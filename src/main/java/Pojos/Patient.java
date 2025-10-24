018d3266d366aff0bbfd7f411ba0c026457bdcce
FMedinaJ <fernando.demedinajunyent@usp.ceu.es>
2025-10-24 17:37:35 +0200

    Doctor and patient pojo

---
+++ src/main/java/Pojos/Patient.java
@@ -0,0 +1,17 @@
+package Pojos;
+
+import java.util.Date;
+
+public class Patient {
+    private int id;
+    private String name;
+    private String surname;
+    private int DNI;
+    private Date dateOfBirth;
+    private String gender;
+    private int phone;
+    private int insurance;
+    private String Email;
+
+
+}
